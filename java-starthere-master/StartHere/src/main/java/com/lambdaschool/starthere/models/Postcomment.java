9ecae9045960cafc3a6ae913ef02b9485d6ab129
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-19 16:02:29 -0700

    postcomments model is pretty close to complete

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Postcomment.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Postcomment.java
@@ -1,14 +1,112 @@
 package com.lambdaschool.starthere.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.lambdaschool.starthere.logging.Loggable;
+import io.swagger.annotations.ApiModelProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 @Loggable
 @Entity
 @Table(name = "postcomments",
-        uniqueConstraints = {@UniqueConstraint(columnNames = {"postid", "postcommentid"})})
-public class Postcomment {
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"userpostid", "postcommentid"})})
+public class Postcomment extends Auditable{
+
+    @ApiModelProperty(name = "postcommentid", value = "primary key for a comment", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long postcommentid;
+
+    @ApiModelProperty(name = "title", value = "Title of post", example = "That's a good point")
+    @Column(nullable = false)
+    private String title;
+
+    @ApiModelProperty(name = "imageurl", value = "Link to image", example = "http://example.com/img.jpg")
+    @Column(nullable = true)
+    private String imageurl;
+
+    @ApiModelProperty(name = "line1", value = "First line of comment", example = "You're right, we should do something!")
+    @Column(nullable = false)
+    private String line1;
+
+    @ManyToOne
+    @JoinColumn(name = "userpostid",
+            nullable = false)
+    @JsonIgnoreProperties("postcomments")
+    private Userpost userpost;
+
+    @ManyToOne
+    @JoinColumn(name = "userid",
+            nullable = false)
+    @JsonIgnoreProperties("postcomments")
+    private User user;
+
+
+    public Postcomment()
+    {
+
+    }
+
+    public Postcomment(User user, Userpost userpost, String title, String line1, String imageurl) {
+        this.user = user;
+        this.title = title;
+        this.imageurl = imageurl;
+        this.line1 = line1;
+        this.userpost = userpost;
+    }
+
+    public Postcomment(User user, Userpost userpost, String title, String line1) {
+        this.user = user;
+        this.title = title;
+        this.line1 = line1;
+        this.userpost = userpost;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getPostcommentid() {
+        return postcommentid;
+    }
+
+    public void setPostcommentid(long postcommentid) {
+        this.postcommentid = postcommentid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public Userpost getUserpost() {
+        return userpost;
+    }
+
+    public void setUserpost(Userpost userpost) {
+        this.userpost = userpost;
+    }
 }
