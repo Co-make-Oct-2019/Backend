3f5745d2682facca9060026a6cdd0c10b94dc07b
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-19 13:05:38 -0700

    adding profile variables to user model

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/User.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/User.java
@@ -27,6 +27,26 @@ public class User extends Auditable
             unique = true)
     private String username;
 
+    @Column(nullable = true,
+            unique = false)
+    private String line1;
+
+    @Column(nullable = true,
+            unique = false)
+    private String line2;
+
+    @Column(nullable = true,
+            unique = false)
+    private String line3;
+
+    @Column(nullable = true,
+            unique = false)
+    private String line4;
+
+    @Column(nullable = true,
+            unique = false)
+    private String imageurl;
+
     @Column(nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
@@ -134,7 +154,47 @@ public class User extends Auditable
         this.userroles = userroles;
     }
 
-//    public List<Useremail> getUseremails()
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getLine3() {
+        return line3;
+    }
+
+    public void setLine3(String line3) {
+        this.line3 = line3;
+    }
+
+    public String getLine4() {
+        return line4;
+    }
+
+    public void setLine4(String line4) {
+        this.line4 = line4;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    //    public List<Useremail> getUseremails()
 //    {
 //        return useremails;
 //    }
