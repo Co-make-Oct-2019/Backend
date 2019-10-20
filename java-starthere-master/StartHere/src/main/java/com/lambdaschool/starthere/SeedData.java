16f16369d0372822d2c11f0044178da94b828ada
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-19 21:41:30 -0700

    getuserinfo and cleaning up code

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/SeedData.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/SeedData.java
@@ -54,7 +54,6 @@ public class SeedData implements CommandLineRunner
                                  r3));
         User u1 = new User("admin",
                            "password",
-//                           "admin@lambdaschool.local",
                            admins, "Miami");
                 u1.getUserposts()
           .add(new Userpost(u1,
@@ -62,29 +61,16 @@ public class SeedData implements CommandLineRunner
         u1.getUserposts()
                 .add(new Userpost(u1,
             "The park is too muddy", "Miami", "If we planted grass, it would help with all the mud. Then it would be fun to play there.", nameFaker.internet().avatar()));
-//        User user, String title, long zip, String line1, String imageurl
 
-
-
-//        u1.getUseremails()
-//          .add(new Useremail(u1,
-//                             "admin@email.local"));
-//        u1.getUseremails()
-//          .add(new Useremail(u1,
-//                             "admin@mymail.local"));
         User firstUser = new User();
         firstUser = userService.save(u1);
 
-//        Userpost up1 = new Userpost();
-//        User thisUser = new User();
-//        thisUser = userService.findByName("admin");
 
-//        up1 = userpostService.findUserpostById(29);
         firstUser.getUserposts().get(0)
         .getPostcomments().add(new Postcomment(firstUser, firstUser.getUserposts().get(0), "There's more in front of my house too!", "You're right. This is a big problem! Let's talk to the mayor.", nameFaker.internet().avatar()));
-//        userpostService.save(up1);
 
-        // data, user
+
+
         ArrayList<UserRoles> datas = new ArrayList<>();
         datas.add(new UserRoles(new User(),
                                 r3));
@@ -92,7 +78,6 @@ public class SeedData implements CommandLineRunner
                                 r2));
         User u2 = new User("cinnamon",
                            "1234567",
-//                           "cinnamon@lambdaschool.local",
                            datas, "Miami");
 
         u2.getUserposts()
@@ -101,15 +86,7 @@ public class SeedData implements CommandLineRunner
         u2.getUserposts()
                 .add(new Userpost(u2,
                         "I lost my dog!", "Miami", "His name is Joe"));
-//        u2.getUseremails()
-//          .add(new Useremail(u2,
-//                             "cinnamon@mymail.local"));
-//        u2.getUseremails()
-//          .add(new Useremail(u2,
-//                             "hops@mymail.local"));
-//        u2.getUseremails()
-//          .add(new Useremail(u2,
-//                             "bunny@email.local"));
+
         userService.save(u2);
 
         // user
@@ -118,15 +95,11 @@ public class SeedData implements CommandLineRunner
                                 r2));
         User u3 = new User("barnbarn",
                            "ILuvM4th!",
-//                           "barnbarn@lambdaschool.local",
                            users, "Miami");
 
         u3.getUserposts()
                 .add(new Userpost(u3,
                         "Town center mural", "Miami", "The mural is old and faded. Let's get a group together to repaint it.", nameFaker.internet().avatar()));
-//        u3.getUseremails()
-//          .add(new Useremail(u3,
-//                             "barnbarn@email.local"));
         userService.save(u3);
 
         users = new ArrayList<>();
@@ -134,7 +107,6 @@ public class SeedData implements CommandLineRunner
                                 r2));
         User u4 = new User("puttat",
                            "password",
-//                           "puttat@school.lambda",
                            users, "Miami");
         userService.save(u4);
 
@@ -143,14 +115,8 @@ public class SeedData implements CommandLineRunner
                                 r2));
         User u5 = new User("misskitty",
                            "password",
-//                           "misskitty@school.lambda",
                            users, "New York");
         userService.save(u5);
-
-        // using JavaFaker create a bunch of regular users
-        // https://www.baeldung.com/java-faker
-        // https://www.baeldung.com/regular-expressions-java
-
 
 
         for (int i = 0; i < 10; i++)
@@ -174,7 +140,7 @@ public class SeedData implements CommandLineRunner
             newUser.getUserposts()
                     .get(0)
                     .getPostcomments()
-                    .add(new Postcomment(newUser, newUser.getUserposts().get(0), nameFaker.backToTheFuture().quote(), nameFaker.rickAndMorty().quote(), nameFaker.internet().avatar()));
+                    .add(new Postcomment(newUser, newUser.getUserposts().get(0), nameFaker.company().bs(), nameFaker.rickAndMorty().quote(), nameFaker.internet().avatar()));
         }
     }
 }
